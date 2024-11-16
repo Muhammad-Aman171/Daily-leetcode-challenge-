ab935df2ecffd779a8ba1d0bb9fe6a84db750cb2
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-16 16:55:14 +0500

    programs

--- day1/programs.js
+++ day1/programs.js
@@ -53,19 +53,14 @@ if (grade >= 90) {
   console.log("your grade is A+");
 } else if (grade >= 80 && grade < 89) {
   console.log("your grade is A");
-}else if (grade >= 70 && grade < 79) {
+} else if (grade >= 70 && grade < 79) {
   console.log("your grade is B");
-}else if (grade >= 60 && grade < 69) {
+} else if (grade >= 60 && grade < 69) {
   console.log("your grade is C");
-}else if (grade >= 50 && grade < 59) {
+} else if (grade >= 50 && grade < 59) {
   console.log("your grade is D");
-}else if (grade >= 40 && grade < 49) {
+} else if (grade >= 40 && grade < 49) {
   console.log("your grade is E");
-}else{
+} else {
   console.log("Congratulation! your are fail");
-  
 }
-
-
-
-
