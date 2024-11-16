6577284d55714bb07d340087bfd3843b3b9450f5
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-16 14:43:58 +0500

    programs

---
+++ day1/programs.js
@@ -0,0 +1,25 @@
+
+// 1. write a program that check the number is positive, nagetive and zero
+
+// userInput = +prompt("enter your number");
+
+// if (userInput > 0) {
+//   console.log("your number is positive");
+// } else if (userInput == 0) {
+//   console.log("your number is equal to zero");
+// }else{
+//     console.log("your number is nagetive");
+// }
+
+// 2. write a program that give the input from the user and tell 
+//    that the user are senior citizen, adult and minor age.
+
+userInput = +prompt("enter your number");
+
+if (userInput >= 50) {
+  console.log("your are a senior citizen");
+} else if (userInput >= 18 && userInput <= 49) {
+  console.log("your are a adult man");
+}else{
+    console.log("your are a minor age");
+}
