37415c799fee37225a2d44857ca53c0876c00fc2
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-16 14:46:20 +0500

    programs

--- day1/programs.js
+++ day1/programs.js
@@ -14,12 +14,17 @@
 // 2. write a program that give the input from the user and tell 
 //    that the user are senior citizen, adult and minor age.
 
-userInput = +prompt("enter your number");
+// userInput = +prompt("enter your number");
 
-if (userInput >= 50) {
-  console.log("your are a senior citizen");
-} else if (userInput >= 18 && userInput <= 49) {
-  console.log("your are a adult man");
-}else{
-    console.log("your are a minor age");
-}
+// if (userInput >= 50) {
+//   console.log("your are a senior citizen");
+// } else if (userInput >= 18 && userInput <= 49) {
+//   console.log("your are a adult man");
+// }else{
+//     console.log("your are a minor age");
+// }
+
+
+// 3. Write a program that checks if a given year is a leap year.
+
+userInput = +prompt("enter your number");
