02d9e4d21d9f1bfa2b41cb9931b25de3a3c0ad71
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-16 16:50:18 +0500

    programs

--- day1/programs.js
+++ day1/programs.js
@@ -1,4 +1,3 @@
-
 // 1. write a program that check the number is positive, nagetive and zero
 
 // userInput = +prompt("enter your number");
@@ -11,7 +10,7 @@
 //     console.log("your number is nagetive");
 // }
 
-// 2. write a program that give the input from the user and tell 
+// 2. write a program that give the input from the user and tell
 //    that the user are senior citizen, adult and minor age.
 
 // userInput = +prompt("enter your number");
@@ -24,7 +23,6 @@
 //     console.log("your are a minor age");
 // }
 
-
 // 3. Write a program that checks if a given year is a leap year.
 
 // userInput = +prompt("enter your number");
@@ -36,17 +34,21 @@
 //   alert("this year is not a leap year")
 // }
 
-
 // 4. Take an integer and check if it’s even or odd.
 
 // userInput = +prompt("enter your number");
 
 // if (userInput % 2 == 0 ){
-//   console.log("this number is even"); 
+//   console.log("this number is even");
 // }
 // else{
-//   console.log("this number is odd"); 
+//   console.log("this number is odd");
 // }
 
-
 // 5. Ask the user for a grade percentage and display the corresponding letter grade (A, B, C, D, F).
+
+grade = +prompt("enter your number");
+
+if (grade >= 90) {
+  
+}
