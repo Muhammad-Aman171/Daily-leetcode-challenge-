3eb388fee0359f504febf93e531e276bb7991caf
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-16 22:36:11 +0500

    programs

--- day1/programs.js
+++ day1/programs.js
@@ -138,11 +138,11 @@
 // 20. Create a program that evaluates if an inputted number is prime.
 
 let userInput = +prompt("enter your number");
-let isPrime = true;
 
 if (userInput < 2) {
   console.log("this number is smaller then 2");
 } else {
+  let isPrime = true;
   for (let i = 2; i <= Math.sqrt(userInput); i++) {
     if (userInput % i == 0) {
       console.log("this number is prime number");
@@ -150,9 +150,9 @@ if (userInput < 2) {
       break;
     }
   }
-}
-if (isPrime) {
-  console.log("this number is prime number");
-} else {
-  console.log("this number is not prime number");
+  if (isPrime) {
+    console.log("this number is prime number");
+  } else {
+    console.log("this number is not prime number");
+  }
 }
