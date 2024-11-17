acb34be28bd737f102991fb17ded88d6cb6b2433
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-17 10:29:41 +0500

    programs

--- day2/programs.js
+++ day2/programs.js
@@ -1,16 +1,27 @@
 // Fibonacci Number Check:
 
-let checkFibonacci = +prompt("enter your number ");
+// let checkFibonacci = +prompt("enter your number ");
 
-fibArr = [0, 1];
+// fibArr = [0, 1];
 
-for (let i = 2; ; i++) {
-  nextFib = fibArr[i - 1] + fibArr[i - 2];
-  if (nextFib > checkFibonacci) break;
-  fibArr.push(nextFib);
+// for (let i = 2; ; i++) {
+//   nextFib = fibArr[i - 1] + fibArr[i - 2];
+//   if (nextFib > checkFibonacci) break;
+//   fibArr.push(nextFib);
+// }
+// if (fibArr.includes(checkFibonacci)) {
+//   console.log("this number is fibonacci number");
+// } else {
+//   console.log("this number is not fibonacci number");
+// }
+
+// Armstrong Number Range:
+
+let ArmstrongNum = prompt("enter your number");
+sum =0
+for (let i = 0; i < ArmstrongNum.length; i++) {
+    mul= ArmstrongNum[i] ** ArmstrongNum.length
+    sum += mul 
 }
-if (fibArr.includes(checkFibonacci)) {
-  console.log("this number is fibonacci number");
-} else {
-  console.log("this number is not fibonacci number");
-}
+console.log(sum);
+
