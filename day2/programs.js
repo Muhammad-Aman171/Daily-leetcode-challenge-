3ee3a6abbba0614aef9f92a19d1d611d9abb9ae7
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-17 07:34:15 +0500

    programs

--- day2/programs.js
+++ day2/programs.js
@@ -3,10 +3,21 @@
 let checkFibonacci = +prompt("enter your number ");
 
 fibArr = [0, 1];
+let isFibonacci = true;
 
 for (let i = 2; i < checkFibonacci; i++) {
   fibArr.push(fibArr[i - 1] + fibArr[i - 2]);
-  if (checkFibonacci === fibArr[i]) {
-    console.log(fibArr);
+  if (checkFibonacci == fibArr[i]) {
+    // console.log("this number is fibonacci number");
+    isFibonacci = true;
+    break;
+  } else {
+    isFibonacci = false;
+    break;
   }
+}
+if (isFibonacci) {
+  console.log("this number is fibonacci number");
+} else {
+  console.log("this number is not fibonacci number");
 }
