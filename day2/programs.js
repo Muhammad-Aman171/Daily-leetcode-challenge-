66aa78d388245ef71ec876c1574dd7b4eb35744b
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-17 07:16:19 +0500

    programs

--- day2/programs.js
+++ day2/programs.js
@@ -4,4 +4,8 @@ let checkFibonacci = +prompt("enter your number ");
 
 fibArr = [0, 1];
 
+for (let i = 2; i < checkFibonacci; i++) {
+    fibArr.push(fibArr[i - 1]+ fibArr[i - 2])
+}
 
+console.log(fibArr);
