c7579b32b3b444fca01acba5a1b1474706514ddb
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-21 01:07:14 +0500

    working

--- day4/programs.js
+++ day4/programs.js
@@ -1,14 +1,26 @@
 // Prime Factors:
 
-let primeNum = +prompt("enter your number");
+// let primeNum = +prompt("enter your number");
 
-let arr = [];
+// let arr = [];
 
-for (let i = 2; i <= primeNum; i++) {
-  while (primeNum % i === 0) {
-    arr.push(i);
-    primeNum /= i;
+// for (let i = 2; i <= primeNum; i++) {
+//   while (primeNum % i === 0) {
+//     arr.push(i);
+//     primeNum /= i;
+//   }
+// }
+
+// console.log(arr);
+
+// Pattern Printing:
+
+let userInput = +prompt("enter your number:");
+let a = "*";
+
+for (let i = 1; i <= userInput; i++) {
+  for (let j = 0; j < i; j++) {
+    console.log(a);
   }
+  console.log("\t");
 }
-
-console.log(arr);
