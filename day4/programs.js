c143217dd96ef7a61094a5bd2fa1e7eadf2101ae
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-21 00:44:59 +0500

    working

--- day4/programs.js
+++ day4/programs.js
@@ -4,12 +4,10 @@ let primeNum = +prompt("enter your number");
 
 arr = [];
 
-for (let i = 1; i < primeNum; i++) {
-  while (primeNum % i == 0) {
-    arr.push(primeNum / i);
-    if (primeNum == 1) {
-      break;
-    }
+for( let i = 1 ; i <= primeNum ; i++){
+  while (primeNum % i == 0){
+    arr.push()
   }
 }
+
 console.log(arr);
