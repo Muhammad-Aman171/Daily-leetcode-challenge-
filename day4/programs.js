0753635252c2d125ff0fab3811a7277a749a3a75
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-21 23:36:21 +0500

    working

--- day4/programs.js
+++ day4/programs.js
@@ -44,11 +44,33 @@
 
 // Find Missing Number:
 
-let arr = [1, 2, 4, 5, 7, 9];
-for (let k = 0; k < 10; k++) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== k) {
-      console.log(i);
-    }
+// const arr = [1, 2, 4, 5, 7, 9];
+// const maxNum = 10;
+// const missingNum = [];
+
+// const sequenceSet = new Set(arr);
+
+// for (let i = 1; i <= maxNum; i++) {
+//   if (!sequenceSet.has(i)) {
+//     missingNum.push(i);
+//   }
+// }
+
+
+
+
+const sequence = [1,5, 6, 8, 10]; // Example sequence
+const maxNumber = 10; // The complete sequence should go up to this number
+const missingNumbers = []; // To store missing numbers
+
+// Create a set of the given sequence for quick lookup
+const sequenceSet = new Set(sequence);
+
+// Check for missing numbers
+for (let i = 1; i <= maxNumber; i++) {
+  if (!sequenceSet.has(i)) { // If the number is not in the sequence
+    missingNumbers.push(i); // Add it to the missing numbers list
   }
 }
+
+console.log("The missing numbers are:", missingNumbers);
