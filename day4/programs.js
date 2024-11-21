d315c34ee00de5e7360bfa7059721e01c0768118
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-21 14:59:21 +0500

    working

--- day4/programs.js
+++ day4/programs.js
@@ -44,5 +44,15 @@
 // const result = sumUntilSingleDigit(number);
 // console.log(`The single-digit sum of ${number} is ${result}`);
 
+let num = +prompt("enter your number ");
 
+while (num >= 10) {
+  let sum = 0;
+  while (num > 0) {
+    sum += num % 10;
+    num = Math.floor(num / 10);
+  }
+  num = sum;
+}
 
+console.log(num);
