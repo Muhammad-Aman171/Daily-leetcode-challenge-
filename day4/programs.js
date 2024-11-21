0bfd165707b8457356e4b69c21181a0620e40752
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-21 14:54:09 +0500

    working

--- day4/programs.js
+++ day4/programs.js
@@ -27,19 +27,22 @@
 
 // Sum of Digits Until Single Digit:
 
-function sumUntilSingleDigit(num) {
-  while (num >= 10) {
-    let sum = 0;
-    while (num > 0) {
-      sum += num % 10; // Extract the last digit and add to the sum
-      num = Math.floor(num / 10); // Remove the last digit
-    }
-    num = sum; // Update the number with the sum of its digits
-  }
-  return num; // Return the single-digit result
-}
+// function sumUntilSingleDigit(num) {
+//   while (num >= 10) {
+//     let sum = 0;
+//     while (num > 0) {
+//       sum += num % 10;
+//       num = Math.floor(num / 10);
+//     }
+//     num = sum;
+//   }
+//   return num;
+// }
 
-// Example usage
-const number = +prompt("enter your number");
-const result = sumUntilSingleDigit(number);
-console.log(`The single-digit sum of ${number} is ${result}`);
+// // Example usage
+// const number = +prompt("enter your number");
+// const result = sumUntilSingleDigit(number);
+// console.log(`The single-digit sum of ${number} is ${result}`);
+
+
+
