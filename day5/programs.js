5104683dfec99c8efa74f9d2d95043a56cd80c86
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-23 22:45:07 +0500

    working

--- day5/programs.js
+++ day5/programs.js
@@ -1,3 +1,11 @@
-let arr = [1, 2, 3];
-arr[5] = 10;
-console.log(arr.length);
+function getAverage(scores) {
+  for (let i = 0; i <= scores; i++) {
+    sumOfAverage = 0;
+    sumOfAverage += i;
+    total = sumOfAverage / scores.lenght;
+    return total;
+  }
+}
+
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
