365997e2f59b201052ba00008d7f2da7c9f44d1c
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-25 09:46:41 +0500

    working

--- day6/programs.js
+++ day6/programs.js
@@ -1,13 +1,18 @@
 // average of students numbers
 
-function getAverage(scores) {
-  sumOfScores = 0;
-  for (let i = 0; i < scores.length; i++) {
-    sumOfScores += scores[i];
-  }
-  total = sumOfScores / scores.length
-    return total;
-}
+// function getAverage(scores) {
+//   sumOfScores = 0;
+//   for (let i = 0; i < scores.length; i++) {
+//     sumOfScores += scores[i];
+//   }
+//   total = sumOfScores / scores.length
+//     return total;
+// }
 
-console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+// console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+// console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+
+
+
+
