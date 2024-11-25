b222f96377dcaf86b49821876691f477843f8fa4
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-25 21:56:02 +0500

    Counter-funct

--- day3/to-be-or-not-to-be.js
+++ day3/to-be-or-not-to-be.js
@@ -15,7 +15,7 @@
  * @param {string} val
  * @return {Object}
  */
-var expect = function (val) {};
+// var expect = function (val) {};
 
 expect(5).toBe(5); // true
 expect(5).notToBe(5); // throws "Equal"
