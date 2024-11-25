91833831570a53fc1550cd1989c9e5fe292b557f
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-25 23:11:52 +0500

    Counter

--- day3/to-be-or-not-to-be.js
+++ day3/to-be-or-not-to-be.js
@@ -24,13 +24,13 @@ var expect = function (val) {
         throw new Error("Not Equal");
       }
     },
-    notToBe: function (n) {
-      if (val !== n) {
-        return true;
-      } else {
-        throw new Error("Equal");
-      }
-    },
+    // notToBe: function (n) {
+    //   if (val !== n) {
+    //     return true;
+    //   } else {
+    //     throw new Error("Equal");
+    //   }
+    // },
   };
 };
 
