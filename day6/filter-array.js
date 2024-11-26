57f4ba9611545a62d024d1e93fba7d89ec290217
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-26 22:16:41 +0500

    Filter Elements from Array

--- day6/filter-array.js
+++ day6/filter-array.js
@@ -29,19 +29,3 @@ var filter = function (arr, fn) {
   }
   return filteredArr;
 };
-
-// var filter = function (arr, fn) {
-//   let newArray = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     newArray.push(fn(para1[i], i));
-//   }
-//   return newArray;
-// };
-
-// let para2 = function (i) {
-//   return i * i;
-// };
-
-// let para1 = [1, 2, 3, 4, 5];
-
-// console.log(filter(para1, para2));
