d7a0b5a992f5b3d839085881c125f7deec2db84d
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-26 22:13:02 +0500

    Filter Elements from Array

--- day6/filter-array.js
+++ day6/filter-array.js
@@ -20,29 +20,31 @@
 
 // Solution:
 
+// var filter = function (arr, fn) {
+//   let newArray = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     newArray.push(fn(para1[i], i));
+//   }
+//   return newArray;
+// };
+
+// let para2 = function (i) {
+//   return i * i;
+// };
+
+// let para1 = [1, 2, 3, 4, 5];
+
+// console.log(filter(para1, para2));
+
 var filter = function (arr, fn) {
-  let newArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    newArray.push(fn(para1[i], i));
+  var filteredArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      filteredArr.push(arr[i]);
+    }
   }
-  return newArray;
+  return filteredArr;
 };
-
-let para2 = function (i) {
-  return i * i;
-};
-
-let para1 = [1, 2, 3, 4, 5];
-
-console.log(filter(para1, para2));
-
-
-
-
-
-
-
-
 
 // var map = function (arr, fn) {
 //   let newArray = [];
