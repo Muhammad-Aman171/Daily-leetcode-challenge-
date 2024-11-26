3a53db4cb500f538942118cb443b560cd6067e76
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-26 22:24:20 +0500

    Array Reduce Transformation

---
+++ day7/arrayReduceTransformation.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+var filter = function (arr, fn) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push(fn(para1[i], i));
+  }
+  return newArray;
+};
+
+let para2 = function (i) {
+  return i * i;
+};
+
+let para1 = [1, 2, 3, 4, 5];
+
+console.log(filter(para1, para2));
