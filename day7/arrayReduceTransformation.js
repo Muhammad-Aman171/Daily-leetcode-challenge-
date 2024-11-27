f8a12d5dedb5b22db5b636594dd30b7b006ffc25
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-27 23:26:28 +0500

    working

--- day7/arrayReduceTransformation.js
+++ day7/arrayReduceTransformation.js
@@ -1,27 +1,15 @@
+var filter = function (arr, fn) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push(fn(para1[i], i));
+  }
+  return newArray;
+};
 
+let para2 = function (i) {
+  return i * i;
+};
 
+let para1 = [1, 2, 3, 4, 5];
 
-
-
-
-
-
-
-
-
-
-// var filter = function (arr, fn) {
-//   let newArray = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     newArray.push(fn(para1[i], i));
-//   }
-//   return newArray;
-// };
-
-// let para2 = function (i) {
-//   return i * i;
-// };
-
-// let para1 = [1, 2, 3, 4, 5];
-
-// console.log(filter(para1, para2));
+console.log(filter(para1, para2));
