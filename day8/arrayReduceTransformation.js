05f4fe1ff0cb3847f917b65b4131c9ec2d820bed
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-30 12:43:56 +0500

    working

--- day8/arrayReduceTransformation.js
+++ day8/arrayReduceTransformation.js
@@ -1,32 +1,30 @@
-// 2626. Array Reduce Transformation
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
 
-// Given an integer array nums, a reducer function fn, and an initial value init, return the final result obtained by executing the fn function on each element of the array, sequentially, passing in the return value from the calculation on the preceding element.
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
 
-// This result is achieved through the following operations: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The ultimate value of val is then returned.
-
-// If the length of the array is 0, the function should return init.
-
-// Please solve it without using the built-in Array.reduce method.
+// Please solve it without the built-in Array.map method.
 
 // Example 1:
 
-// Input:
-// nums = [1,2,3,4]
-// fn = function sum(accum, curr) { return accum + curr; }
-// init = 0
-// Output: 10
+// Input: arr = [1,2,3], fn = function plusone(n) { return n + 1; }
+// Output: [2,3,4]
 // Explanation:
-// initially, the value is init=0.
-// (0) + nums[0] = 1
-// (1) + nums[1] = 3
-// (3) + nums[2] = 6
-// (6) + nums[3] = 10
-// The final answer is 10.
+// const newArray = map(arr, plusone); // [2,3,4]
+// The function increases each value in the array by one.
 
-// Solution:
+let map = function (arr, fn) {
+  let a = [];
+  for (let i = 0; i < arr.length; i++) {
+    a.push(fn(arr[i], i));
+  }
+  return a;
+};
 
-// let val = init;
-// for (let i = 0; i < nums.length; i++) {
-//   val = fn(val, nums[i]);
-// }
-// return val;
+let transformationFunc = function (i , n) {
+  return i + n;
+};
+
+let inputArr = [1, 2, 3];
+console.log(inputArr);
+
+console.log(map(inputArr, transformationFunc));
