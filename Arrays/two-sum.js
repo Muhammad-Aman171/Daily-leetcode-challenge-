0cb3f073214b98dd6fab6dc1a1f33feb82b07eeb
Muhammad-Aman171 <ag3734359@gmail.com>
2024-12-12 07:07:21 +0500

    workin

--- Arrays/two-sum.js
+++ Arrays/two-sum.js
@@ -1,28 +1,28 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
 
-var twoSum = function (nums, target) {
-  let newArr = [];
-  for (let i in nums) {
-    for (let h = 1; h < nums.length; h++) {
-      //   newArr.push);
-      if (nums[i] + nums[h] == target) {
-        newArr.push(i, h);
-        break;
-      }
-    }
+// var twoSum = function (nums, target) {
+//   let newArr = [];
+//   for (let i in nums) {
+//     for (let h = 1; h < nums.length; h++) {
+//       //   newArr.push);
+//       if (nums[i] + nums[h] == target) {
+//         newArr.push(i, h);
+//         break;
+//       }
+//     }
 
-    console.log(nums[i]);
-  }
-  return newArr;
-};
+//     console.log(nums[i]);
+//   }
+//   return newArr;
+// };
 
-// let number = [2, 7, 11, 15];
-let number = [3, 2, 4];
-// let number = [2, 7, 11, 15];
-let target = 6;
+// // let number = [2, 7, 11, 15];
+// let number = [3, 2, 4];
+// // let number = [2, 7, 11, 15];
+// let target = 6;
 
-console.log(twoSum(number, target));
+// console.log(twoSum(number, target));
