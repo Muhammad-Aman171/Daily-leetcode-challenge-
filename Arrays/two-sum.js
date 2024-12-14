83f0b0c65555cfb2a4c4cd65f729c9085f81f65d
Muhammad-Aman171 <ag3734359@gmail.com>
2024-12-14 23:53:13 +0500

    Two Sum

--- Arrays/two-sum.js
+++ Arrays/two-sum.js
@@ -5,23 +5,20 @@
  */
 
 var twoSum = function (nums, target) {
-  let newArr = [];
   for (let i = 0; i < nums.length; i++) {
-    for (let h = 1; h < nums.length; h++) {
+    for (let h = i + 1; h < nums.length; h++) {
       if (nums[i] + nums[h] === target) {
-        newArr.push(i, h);
-        break;
+        return [i, h];
       }
     }
-    break;
   }
-  return newArr;
+  return [];
 };
 
-// let number = [2, 7, 11, 15];
-let number = [3, 2, 4];
+let number = [2, 7, 11, 15];
+// let number = [3, 2, 4];
 // let number = [3, 3];
-// let target = 9;
-let target = 6;
+let target = 9;
+// let target = 6;
 
 console.log(twoSum(number, target));
