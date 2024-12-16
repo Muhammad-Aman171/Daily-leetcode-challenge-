a8d5e9ffd1ff34053b25ed4252e5a5d282d8aa6a
Muhammad-Aman171 <ag3734359@gmail.com>
2024-12-17 01:12:21 +0500

    wor

--- Arrays/remove-duplicates-from-sorted-array.js
+++ Arrays/remove-duplicates-from-sorted-array.js
@@ -3,20 +3,20 @@
  * @return {number}
  */
 
-var removeDuplicates = function(nums){
-    let newArr = [];
-    for (const i in nums) {
-        if (!newArr.includes(nums[i])){
-            console.log(nums[i]);
-            
-        }
-        
+var removeDuplicates = function (nums) {
+  let newArr = [];
+  for (const i in nums) {
+    if (!newArr.includes(nums[i])) {
+      newArr.push(nums[i]);
+      console.log(nums[i]);
+      
     }
-    return newArr;
-}
+  }
+  return newArr.length;
+};
 
-
-let  nums = [1,1,2]
+let nums = [1, 1, 2];
 // let nums= [0,0,1,1,1,2,2,3,3,4]
 
 console.log(removeDuplicates(nums));
+
