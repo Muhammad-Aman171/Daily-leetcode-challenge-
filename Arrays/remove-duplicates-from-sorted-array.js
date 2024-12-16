04510f7bbcc1e390e5af6082f3d205ca00359902
Muhammad-Aman171 <ag3734359@gmail.com>
2024-12-16 22:48:19 +0500

    worki

--- Arrays/remove-duplicates-from-sorted-array.js
+++ Arrays/remove-duplicates-from-sorted-array.js
@@ -1,1 +1,16 @@
-// let a = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var removeDuplicates = function(nums){
+    let k = 0;
+
+    return k;
+}
+
+
+let  nums = [1,1,2]
+// let nums= [0,0,1,1,1,2,2,3,3,4]
+
+console.log(removeDuplicates(nums));
