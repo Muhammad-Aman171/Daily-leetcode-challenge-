037caec03e3f30ed1190aa551525bad6fc22292e
Muhammad-Aman171 <ag3734359@gmail.com>
2024-12-18 11:02:12 +0500

    workin

--- Arrays/research-insert-position.js
+++ Arrays/research-insert-position.js
@@ -1,1 +1,14 @@
-let a = 0
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+var searchInsert = function (nums, target) {
+  return k;
+};
+
+let nums = [1, 3, 5, 6];
+let target = 5;
+
+console.log(searchInsert(nums, target));
