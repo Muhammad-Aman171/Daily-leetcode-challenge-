bd56ca04ec7aa32842c3d8cfeac157d6f74cd19d
Muhammad-Aman171 <ag3734359@gmail.com>
2024-12-20 12:52:57 +0500

    work

--- Arrays/plus-one.js
+++ Arrays/plus-one.js
@@ -5,7 +5,7 @@
 
 var plusOne = function (digits) {
   let n = digits.length;
-  for (const i = n - 1; i >= 0; i--) {
+  for (let i = n - 1; i >= 0; i--) {
     if (digits[i] < 9) {
       digits[i]++;
       return digits;
@@ -13,9 +13,11 @@ var plusOne = function (digits) {
       digits[i] = 0;
     }
   }
+
   return digits;
 };
 
-let digits = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3];
+// let digits = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3];
+let digits = [9, 9];
 
 console.log(plusOne(digits));
