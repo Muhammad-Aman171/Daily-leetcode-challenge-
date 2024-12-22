0ed3d136cbd50584531848f80a9ab7f91a62e00a
Muhammad-Aman171 <ag3734359@gmail.com>
2024-12-22 14:51:26 +0500

    working

--- Arrays/merge-sorted-array.js
+++ Arrays/merge-sorted-array.js
@@ -8,21 +8,23 @@
 
 var merge = function (nums1, m, nums2, n) {
   let a = nums2.length - 1;
-  for (let i = nums1.length - 1; i > n; i-- ) {
-    nums1[i] = nums2[a];
-    a--;
-    n++
+  for (let i = nums1.length - 1; i >= n; i--) {
+    if (nums1[i] == 0) {
+      nums1[i] = nums2[a];
+      a--;
+      n++;
+    }
   }
   nums1.sort((a, b) => a - b);
   return nums1;
 };
 
-// let nums1 = [1, 2, 3, 0, 0, 0];
+let nums1 = [1, 2, 3, 0, 0, 0];
 let m = 3;
-// let nums2 = [2, 5, 6];
+let nums2 = [2, 5, 6];
 let n = 3;
 
-let nums1 = [-1, 0, 0, 3, 3, 3, 0, 0, 0];
-let nums2 = [1, 2, 2];
+// let nums1 = [-1, 0, 0, 3, 3, 3, 0, 0, 0];
+// let nums2 = [1, 2, 2];
 
 console.log(merge(nums1, m, nums2, n));
