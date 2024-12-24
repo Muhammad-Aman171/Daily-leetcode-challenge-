08bd689c5d428b106bcb15345493aeddf1d4d69a
Muhammad-Aman171 <ag3734359@gmail.com>
2024-12-24 23:46:29 +0500

    wokrin

--- Arrays/single-number.js
+++ Arrays/single-number.js
@@ -3,6 +3,17 @@
  * @return {number}
  */
 
-var singleNumber = function(nums){
-    
-}
+var singleNumber = function (nums) {
+  let number = 0;
+  for (const i in nums) {
+    for (let j = 1; j < nums.length; j++) {
+      if (nums[i] !== nums[j]) {
+        console.log(nums[i]);
+      }
+    }
+  }
+  return number;
+};
+
+let nums = [2, 2, 1];
+console.log(singleNumber(nums));
