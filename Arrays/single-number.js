d4ff79a2b3ed16eaf31964d774739137df984eca
Muhammad-Aman171 <ag3734359@gmail.com>
2024-12-25 00:01:02 +0500

    wokri

--- Arrays/single-number.js
+++ Arrays/single-number.js
@@ -5,10 +5,13 @@
 
 var singleNumber = function (nums) {
   let number = 0;
-  for (const i in nums) {
+  for (let i = 0; i < nums.length; i++) {
     for (let j = 1; j < nums.length; j++) {
       if (nums[i] !== nums[j]) {
         console.log(nums[i]);
+        number++;
+      } else {
+        break;
       }
     }
   }
