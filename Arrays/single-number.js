4244274fea57ad004682a26850e823a056633048
Muhammad-Aman171 <ag3734359@gmail.com>
2024-12-25 17:09:58 +0500

    working

--- Arrays/single-number.js
+++ Arrays/single-number.js
@@ -5,14 +5,14 @@
 
 var singleNumber = function (nums) {
   let number = 0;
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 1; j < nums.length; j++) {
-      if (nums[i] !== nums[j]) {
-        console.log(nums[i]);
-        number++;
-      } else {
-        break;
-      }
+  // for (let i = 0; i < nums.length; i++) {
+  //   for (let j = 1; j < nums.length; j++) {
+  //     if (nums[i] !== nums[j]) {
+  //       console.log(nums[i]);
+  //       number++;
+  //     } else {
+  //       break;
+  //     }
     }
   }
   return number;
